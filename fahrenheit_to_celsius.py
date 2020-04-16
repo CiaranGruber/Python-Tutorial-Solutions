b72e809a46f44959cb9877c7effba86b42b39260
Ciaran Gruber <ciaran.gruber@my.jcu.edu.au>
2020-04-16 15:55:37 +1000

    Create solution for "Convert Fahrenheit to Celsius"

---
+++ fahrenheit_to_celsius.py
@@ -0,0 +1,8 @@
+def main():
+    fahrenheit = int(input("Fahrenheit value: "))
+    celsius = (fahrenheit - 32) * 5 / 9
+    print(fahrenheit, "fahrenheit is", celsius, "celsius")
+
+
+if __name__ == "__main__":
+    main()
