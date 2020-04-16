53f8d6aaa93c1e59f36eb97723a74f4f9db46785
Ciaran Gruber <ciaran.gruber@my.jcu.edu.au>
2020-04-16 15:58:56 +1000

    Create solution for "Best practice formatting for python"

---
+++ reformat_solution.py
@@ -0,0 +1,11 @@
+def main():
+    num = int(input("Num:"))
+    if num == 5:
+        print("Hello world", num)
+    else:
+        print("Hello, this a very long string that is so long in fact that Pycharm says it extends past what PEP 8 "
+              "allows")  # Note: do this line first
+
+
+if __name__ == "__main__":
+    main()
