d43748c2792cee39d7579bf598ebbaca46dc7cea
Ciaran Gruber <ciaran.gruber@my.jcu.edu.au>
2020-04-16 15:55:01 +1000

    Create solution for "Show the different operations of two numbers"

---
+++ operations.py
@@ -0,0 +1,11 @@
+def main():
+    a = 6
+    b = 3
+    print("Sum:", a + b)
+    print("Difference:", abs(a - b))
+    print("Product:", a * b)
+    print("Quotient:", a / b)
+
+
+if __name__ == "__main__":
+    main()
